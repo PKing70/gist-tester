345b9bc5eea1c1041548ccf09cf9c80dba762fbe
Patrick King <pking@splunk.com>
2025-08-13 12:09:26 -0700

    Revert "Adding markdown"
    This reverts commit 38faf3635accd16b100b044a1f422f0d8f9e9f68.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,14 +6,5 @@ module.exports = {
     title: `GistTester`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/src/content`,
-      },
-    },
-    `gatsby-transformer-remark`,    
-  ],
+  plugins: [],
 }
